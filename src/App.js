115bac5768850c4c0549bac97e6472c898b424a0
j3n4r3v <jene4ka4reva@gmail.com>
2021-09-03 19:16:40 +0300

    1 step in html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header className="header">
+      {/* <div className="container"> */}
+        <div>
+          <svg></svg>
+          <Link className="" to=""></Link>
+        </div>
+        <ul className="navigation">
+            <li className="navigation">
+              <Link className="navigation" to="#"></Link>
+          </li>
+            <li className="navigation">
+              <Link className="navigation" to="#"></Link>
+          </li>
+            <li className="navigation">
+              <Link className="navigation" to="#"></Link>
+          </li>
+            <li className="navigation">
+              <Link className="navigation" to="#"></Link>
+          </li>
+            <li className="navigation">
+              <Link className="navigation" to="#"></Link>
+          </li>
+            <li className="navigation">
+              <img className="navigation" src="" alt=""></img>
+              <button className="navigation">
+                <span className="navigation"></span>
+              </button>
+          </li>
+        </ul>
+      {/* </div> */}
+    </header>
+      <section className="advertisment">
+        <div className="advertisment">
+          <h1 className="advertisment">Лига Банк</h1>
+          <p className="advertisment"></p>
+          <Link className="advertisment" href=""></Link>
+  </div>
+        <div className="advertisment">
+          <img className="advertisment" src="" alt=""></img>
+          <img className="advertisment" src="" alt=""></img>
+      </div>
+      </section>
+      <section className="history">
+        <h3 className="history">Конвертер валют</h3>
+        <div className="history">
+          <span className="history"></span>
+          <span className="history"></span>
+</div>
+        <input className="history" type="number"></input>
+        <select className="history" name="" id=""></select>
+        <p className="history"></p>
+<div>
+          <input className="history" type="number"></input>
+          <select className="history" name="" id=""></select>
+</div>
+        <div className="history">
+          <input className="history" type="number"></input>
+          <button className="history"></button>
+</div>
+      </section>
+      <section className="history">
+        <h4 className="history">История конвертации</h4>
+        <div className="history">
+          <div className="history">
+            <span className="history"></span>
+            <span className="history"></span>
+            <span className="history"></span>
+            <button className="history"></button>
+          </div>
+          <div className="history">
+            <span className="history"></span>
+            <span className="history"></span>
+            <span className="history"></span>
+            </div>
+          </div>
+      </section>
+      <footer className="socials">
+        <div className="socials">
+          <div className="socials">
+            <img className="socials" src="" alt=""></img>
+            <span className="socials"></span>
+          </div>
+          <p className="socials"></p>
+        </div>
+        <ul className="socials">
+          <li className="socials">
+            <Link className="socials" href="">Услуги</Link></li>
+          <li className="socials">
+            <Link className="socials" href="">Расчитать кредит</Link></li>
+          <li className="socials">
+            <Link className="socials" href="">Контакты</Link></li>
+          <li className="socials">
+            <Link className="socials" href="">Задать вопрос</Link></li>
+        </ul>
+        <div>
+          <span className="socials"></span>
+          <p className="socials"></p>
+        </div>
+        <div className="socials">
+          <span className="socials"></span>
+          <p className="socials"></p>
+        </div>
+        <ul className="socials">
+          <li className="socials">
+            <Link className="socials" href=""></Link></li>
+          <li className="socials">
+            <Link className="socials" href=""></Link></li>
+          <li className="socials">
+            <Link className="socials" href=""></Link></li>
+          <li className="socials">
+            <Link className="socials" href=""></Link></li>
+        </ul>
+      </footer>
     </div>
+    
+
+    
   );
 }
 
